7bd2750cecaf17b432c16aeabf23f8c4debe6941
Masato Mori <13274198+spica314@users.noreply.github.com>
2024-05-04 15:04:24 +0900

    Use --detach in run-surrealdb-test-container.sh

--- test-tools/run-surrealdb-test-container.sh
+++ test-tools/run-surrealdb-test-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --rm --pull always -p '127.0.0.1:8007:8000' surrealdb/surrealdb:latest start
+sudo docker run --detach --rm --pull always -p '127.0.0.1:8007:8000' surrealdb/surrealdb:latest start
