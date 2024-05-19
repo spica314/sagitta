c7acbc7f60640bbaf8eca7f4a36199b13e3fbeb8
Masato Mori <13274198+spica314@users.noreply.github.com>
2024-05-20 08:55:18 +0900

    Add sagitta-web

---
+++ sagitta-web/src/main.rs
@@ -0,0 +1,31 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use tracing::Level;
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        div {
+            h1 { "Sagitta Web" }
+        }
+    }
+}
